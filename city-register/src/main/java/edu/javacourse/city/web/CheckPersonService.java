2cec6c2b77f4e46d47a3cd0e7ff211ac2e9d1170
Alekxander Monakhov <all.monah.off@gmail.com>
2021-09-26 03:40:33 +0500

    2509/106

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,14 +1,23 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 //resource class
 @Path("/check")
+
 public class CheckPersonService {
-    @GET
-    public String checkPerson(){
-        return "Simple String";
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(PersonRequest request){
+        System.out.println(request.toString());
+        return new PersonResponse();
     }
 }
